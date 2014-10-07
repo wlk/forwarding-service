3afcc98f39c44d698cff5e07b2c497d64e07673f
Wojciech Langiewicz <wlangiewicz@gmail.com>
2014-10-07 20:19:58 +0200

    added addressToKey option to import from hex private key

--- src/main/scala/com/varwise/btc/forwarding/Utils.scala
+++ src/main/scala/com/varwise/btc/forwarding/Utils.scala
@@ -1,12 +1,15 @@
 package com.varwise.btc.forwarding
 
+import java.math.BigInteger
+
 import org.bitcoinj.core.{Base58, DumpedPrivateKey, ECKey, NetworkParameters}
 
 object Utils {
   def addressToKey(params: NetworkParameters, sourceAddress: String): ECKey = {
     sourceAddress match {
-      case _ if sourceAddress.length == 51 || sourceAddress.length == 52 => new DumpedPrivateKey(params, sourceAddress).getKey
-      case _ => ECKey.fromPrivate(Base58.decodeToBigInteger(sourceAddress))
+      case _ if sourceAddress.length == 51 || sourceAddress.length == 52 => new DumpedPrivateKey(params, sourceAddress).getKey //WIF
+      case _ if sourceAddress.length == 64 => ECKey.fromPrivate(new BigInteger(sourceAddress, 16)) //hex encoded private key
+      case _ => ECKey.fromPrivate(Base58.decodeToBigInteger(sourceAddress)) //base58
     }
   }
 
